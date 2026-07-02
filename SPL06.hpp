8ff2b73d4c7ffcdf82479a0653e11f94106a835c
molqzone <toiperus@sohu.com>
2026-07-02 17:32:25 +0800

    fix: use typed LibXR topic creation

--- SPL06.hpp
+++ SPL06.hpp
@@ -75,7 +75,7 @@ class SPL06 : public LibXR::Application {
         const char* data_topic_name, uint32_t sample_period_ms,
         size_t task_stack_depth)
       : sample_period_ms_(sample_period_ms),
-        topic_(data_topic_name, sizeof(data_)),
+        topic_(LibXR::Topic::CreateTopic<Data>(data_topic_name)),
         cs_(hw.template FindOrExit<LibXR::GPIO>({"spl06_cs"})),
         spi_(hw.template FindOrExit<LibXR::SPI>({"spi_spl06", "spi2", "SPI2"})),
         spi_mutex_(hw.template Find<LibXR::Mutex>({"spi2_mutex"})),
